dbfa77cd72f72f55d6f3802a6fd0e3f9315ecce7
Alexkid87IA <alex@nyac.fr>
2025-06-01 12:27:37 +0200

    Corriger le routage des catégories

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ResponsiveNavbar } from './components/layout/ResponsiveNavbar';
 import { Footer } from './components/layout/Footer';
 import { ErrorBoundary } from './components/ErrorBoundary';
